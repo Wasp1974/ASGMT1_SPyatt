fd2a770d02c13fae624bb1b913c37864ea75095d
Lars Jensen <larsjensen@gmx.de>
2015-10-02 11:25:38 +0200

    Update config.php

--- config.php
+++ config.php
@@ -6,8 +6,8 @@
 $user = "root";
 $pass = "";
 $db = new PDO("mysql:host=localhost;dbname=memory",$user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); 
-define("IMG", "../Memory/img/");
-define("AUDIO", "../Memory/audio/");
+define("IMG", "/Memory/img/");
+define("AUDIO", "/Memory/audio/");
 $one_table = array('movies','composer');
 
 
